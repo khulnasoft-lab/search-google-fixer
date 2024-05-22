31a99863affc3bbbea2763af4bde551a847435fd
NxPKG <116948796+NxPKG@users.noreply.github.com>
2024-05-22 20:20:11 +0600

    Create common.js
    Signed-off-by: NxPKG <116948796+NxPKG@users.noreply.github.com>

---
+++ common.js
@@ -0,0 +1,18 @@
+const RunningFirefoxVersion = (navigator.userAgent.match(/Firefox\/([0-9.]+)/) || ["", "58.0"])[1];
+const RunningAndroidVersion = navigator.userAgent.match(/Android [0-9.]+/) || "Android 6.0";
+
+const ChromeMajorVersionToMimic = `${parseInt(RunningFirefoxVersion) + 4}.0.0.0`;
+
+const ChromePhoneUA = `Mozilla/5.0 (Linux; ${RunningAndroidVersion}; Nexus 5 Build/MRA58N) FxQuantum/${RunningFirefoxVersion} AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${ChromeMajorVersionToMimic} Mobile Safari/537.36`;
+const ChromeTabletUA = `Mozilla/5.0 (Linux; ${RunningAndroidVersion}; Nexus 7 Build/JSS15Q) FxQuantum/${RunningFirefoxVersion} AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${ChromeMajorVersionToMimic} Safari/537.36`;
+
+function getUserAgentOverride(userAgent) {
+  if (userAgent.includes("Mobile")) {
+    return ChromePhoneUA;
+  }
+  if (userAgent.includes("Tablet")) {
+    return ChromeTabletUA;
+  }
+  // Desktop mode requested.
+  return userAgent;
+}
